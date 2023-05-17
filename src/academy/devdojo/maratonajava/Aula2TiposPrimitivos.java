c238a684d00af28f6d58284e833bf8e4ac56dd1f
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-05-17 10:07:17 -0300

    Primitivos

---
+++ src/academy/devdojo/maratonajava/Aula2TiposPrimitivos.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava;
+
+public class Aula2TiposPrimitivos {
+    public static void main(String[] args) {
+// int, double, float, char tem tabelas com valores que viram letras ou simbolos dependendo da sua tabela
+// , byte, short, long, boolean
+        // servem para o tamanho do seu código em caracteres como exemplo,
+        // o byte ele só aceita ate 127 ou -128 numeros como 1000 execedem sua
+        // capacidade de armazenamento de memoria.
+
+        int age = 10;
+        long numberBig= 10000;
+        double moneyDouble = 2000;
+        double moneyFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char character = 1220;
+
+        System.out.println("a idade é " + age +  " anos. ");
+        System.out.println(false);
+        System.out.println(character);
+
+    }
+}
--- src/academy/devdojo/maratonajava/OlaRuanJava.java
+++ src/academy/devdojo/maratonajava/OlaRuanJava.java
@@ -1,3 +1,5 @@
+package academy.devdojo.maratonajava;
+
 public class OlaRuanJava {
     public static void main(String [] args){
         System.out.println("Oi meu chapa.");
