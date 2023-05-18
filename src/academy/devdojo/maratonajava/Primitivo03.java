59cb6667aef122db8c3fa614090b2847840ec5d2
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-05-18 11:54:58 -0300

    Primitivos teste

---
+++ src/academy/devdojo/maratonajava/Primitivo03.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava;
+
+public class Primitivo03 {
+    public static void main(String[] args) {
+        String nome = "Ruan";
+        String endereco = "Av Brasil 569";
+        double salario = 3500.12;
+        String data = "18/05/2023";
+        String relatorio = "Eu "+ nome+" morando no enedereço "+ endereco+" confirmo que recebi o salário de "+ salario+ " na data " + data;
+        System.out.println(relatorio);
+    }
+
+}
