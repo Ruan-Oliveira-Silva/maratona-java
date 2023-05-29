c182754e12a497f16e3f55b28b777f368650e90d
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-05-29 09:56:38 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/Aula04_Operadores.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava;
+
+public class Aula04_Operadores {
+    public static void main(String[] args) {
+        int number01 = 10;
+        int number02 = 20;
+        int result = number02 - number01;
+        double resultdi = number01 / (double) number02;
+        System.out.println(result);
+        System.out.println(resultdi);
+    }
+}
