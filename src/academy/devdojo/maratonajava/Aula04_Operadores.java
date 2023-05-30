11e24ed452a6b64dc26f4d77548de3a979a79f38
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-05-30 08:51:02 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/Aula04_Operadores.java
+++ src/academy/devdojo/maratonajava/Aula04_Operadores.java
@@ -8,5 +8,24 @@ public class Aula04_Operadores {
         double resultdi = number01 / (double) number02;
         System.out.println(result);
         System.out.println(resultdi);
+
+        // %
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // < > <= >=  == !=
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        System.out.println("IsDezMaiorQueVinte " +isDezMaiorQueVinte);
+
+        // Operadores Logicos && ( AND)  || (or) ! (ou)
+        int idade = 25;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta "  +isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " +isDentroDaLeiMenorQueTrinta);
+
     }
 }
--- src/academy/devdojo/maratonajava/Primitivo03.java
+++ src/academy/devdojo/maratonajava/Primitivo03.java
@@ -8,7 +8,7 @@ public class Primitivo03 {
         String data = "18/05/2023";
         String relatorio = "Eu "+ nome+" morando no enedereço "+ endereco+" confirmo que recebi o salário de "+ salario+ " na data " + data;
         System.out.println(relatorio);
-        System.out.println(relatorio);
+
     }
 
 
