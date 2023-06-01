363763f3a2a1e55792786cb6434a7dffda9cb1d5
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-05-31 22:00:59 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/OperadoresDeAtribuição.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava;
+
+public class OperadoresDeAtribuição {
+    // = +=  -= *= %= /=     >>>>>> %= é resto, sendo resto sempre 0.0;
+    public static void main(String[] args) {
+        double bonus = 1800;
+        bonus += 1000;  // 2800 é o valor do bonus nessa linha de código.
+        bonus -= 1000; // 1800 é o valor de bonus nessa linha de código.
+        System.out.println(bonus);
+
+        //
+
+        int contador = 0;
+        contador += 1; // contador = contador +1 ; forma burra de fazer //
+        contador++; // forma mais ou menos esperta;
+        ++contador; // se colocar antes ele incrementa o valor primeiro e depois executa o código
+        // no sout chamando o código contador.
+        int contador2 = 2;
+
+        System.out.println(contador);
+
+
+    }
+}
