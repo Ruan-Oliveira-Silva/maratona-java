a1a368f702c7a3df6ab88fad3824a1eaadb8be50
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-06-01 09:24:21 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/Aula05EstruturasCondicionais02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava;
+
+public class Aula05EstruturasCondicionais02 {
+    public static void main(String[] args) {
+        // idade < 15 categoria infantil
+        // idade >= 15 && idade < 17 categiria juvenil
+        // idade >= 18 categoria adulto
+        // todas as condiçoes das variaveis precisam ser cobertas por if e else para serem usadas.
+        // variaveis locais precisam ser inicializadas precisa ter uma alocação de memoria
+        // como o nosso valor de string categoria.
+
+        int idade = 30;
+        String categoria;
+        if(idade < 15){
+           categoria = "Categoria Infantil ";
+        }else if (idade >= 15 && idade < 18){
+           categoria = "Categoria Juvenil";
+        }else {
+          categoria = "Categoria Adulto ";
+        }
+        System.out.println(categoria);
+    }
+}
