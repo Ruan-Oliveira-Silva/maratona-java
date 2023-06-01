d3a76b894631906e0cbec72ae6b2ea054e11433c
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-05-31 21:51:32 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/Aula04_Operadores.java
+++ src/academy/devdojo/maratonajava/Aula04_Operadores.java
@@ -27,5 +27,17 @@ public class Aula04_Operadores {
         System.out.println("isDentroDaLeiMaiorQueTrinta "  +isDentroDaLeiMaiorQueTrinta);
         System.out.println("isDentroDaLeiMenorQueTrinta " +isDentroDaLeiMenorQueTrinta);
 
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPS5 = 5000F;
+
+        // || PIPE é igual a ou e & como função de comparação. > se alguma condição for verdadeira
+        // do isPS5Compravel como a varaivel valor total conta corrente ou || valor total conta pou
+        // a função faz a comparação e retorno true ou false julgando que o valor possuido é maior
+        // que o valor necessitado nesse caso de 5000 temos 10200 no total poupanca + corrente.
+
+        boolean isPS5Compravel = valorTotalContaCorrente > valorPS5 || valorTotalContaPoupanca > valorPS5;
+        System.out.println("isPS5Compravel " +isPS5Compravel);
+
     }
 }
