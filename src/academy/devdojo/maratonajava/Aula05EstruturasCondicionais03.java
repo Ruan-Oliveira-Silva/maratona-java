bf8722636fe786eac563810d2e54e3fdac23b4bb
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-06-02 10:36:21 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        // doar se salario > 5000
+        // operador ternário tema de aula > 3 condiçoes para ser utilizado (condicao) ? verdadeiro : falso
+        double salario = 5000;
+        String resultado = salario >= 5000 ? "Eu vou doar 500 pro DevDojo " : "Ainda não tenho condições, mas vou ter ! ";
+
+        System.out.println(resultado);
+
+    }
+}
