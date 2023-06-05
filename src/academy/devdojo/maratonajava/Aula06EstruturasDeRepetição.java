70d542c049de6fdb39b3ea87b4da2d9247b864e6
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-06-05 09:54:41 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/Aula06EstruturasDeRepetição.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava;
+
+public class Aula06EstruturasDeRepetição {
+    public static void main(String[] args) {
+        // while, do while, for
+        // while sempre precisa retornar um valor booleano, entao usamos expressoes de comparação;
+        // do while sempre executa seu sout 1 vez antes da sua condição, sendo true ou false n importa.
+        int count = 0;
+        while(count < 10){
+            System.out.println(++count);
+            do{
+                System.out.println("Dentro do do-while" + ++count);
+            }while (count < 10);
+        // for usamos dois ; primeiro para  a variavel, segundo para a comparação e o terceiro
+            // com o que voce quer incrementar como exemplo ++ para adicionar mais um
+
+            for(int i=0;i <10 ;i++){
+                System.out.println("For " +i);
+            }
+
+        }
+    }
+}
---
+++ src/academy/devdojo/maratonajava/EstruturasCOndicionais06.java
@@ -0,0 +1,36 @@
+package academy.devdojo.maratonajava;
+
+public class EstruturasCOndicionais06 {
+    public static void main(String[] args) {
+        // dados os valores de 1 a 7, imprima se é dia
+        // útil ou final de semana
+        byte dia = 1;
+        switch(dia){
+            case 1:
+                System.out.println("Domingo é fim de semana !");
+                break;
+            case 2:
+                System.out.println("Segunda é foda, mas é dia útil !");
+                break;
+            case 3:
+                System.out.println("Terça é dia útil !");
+                break;
+            case 4:
+                System.out.println("Quarta é dia útil !");
+                break;
+            case 5:
+                System.out.println("Quinta é dia útil !");
+                break;
+            case 6:
+                System.out.println("Sexta é quase fim de semana !");
+                break;
+            case 7:
+                System.out.println("Sabado é fim de semana  !");
+                break;
+            default:
+                System.out.println("Inválido  !");
+                break;
+
+        }
+    }
+}
