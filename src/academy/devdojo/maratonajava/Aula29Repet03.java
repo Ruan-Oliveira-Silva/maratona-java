1357a77f80afa6d92127423336ffac8a7ba6ee04
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-06-06 10:13:47 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/Aula29Repet03.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava;
+
+public class Aula29Repet03 {
+    // imprima os primeiros 25 num de um dado valor
+    //exemplo 50
+    // um break precisa estar dentro de um switch ou de um loop para nao quebrar o codigo.
+    public static void main(String[] args) {
+        int valorMax = 50;
+       for (int i = 0;i < valorMax ; i++ ){
+           if (i > 25){
+               break;
+           }
+           System.out.println(i);
+        }
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/ExercicioAula06.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava;
+
+public class ExercicioAula06 {
+    public static void main(String[] args) {
+        // repetir numeros pares de 0 a 1000000
+
+        for (int i = 0; i <= 100000; i++) {
+            if( i % 2 == 0)
+            System.out.println(i);
+
+        }
+
+
+    }
+}
