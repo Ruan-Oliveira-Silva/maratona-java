599b4a993ab6667ddfcb1ec0f573ff440fe6354b
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-06-12 09:33:11 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/Arrays2.java
+++ src/academy/devdojo/maratonajava/Arrays2.java
@@ -2,10 +2,11 @@ package academy.devdojo.maratonajava;
 
 public class Arrays2 {
     public static void main(String[] args) {
-        // byte, short, int, long, float, double = 0
-        // char '\u000' ' '
-        // boolean false
-        // String null
+        /*
+         byte, short, int, long, float, double = 0
+         boolean false
+         String null
+        */
 
         int[] idades = new int[3];
         System.out.println(idades[2]);
