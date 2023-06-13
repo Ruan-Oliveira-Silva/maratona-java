fa3f026759f7ec817f67f0c87bd965c1664c42c9
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-06-13 08:25:08 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/ArryasMultdimen02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava;
+
+public class ArryasMultdimen02 {
+    public static void main(String[] args) {
+        int[][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = new int[3];
+
+        for (int[] arrayBase : arrayInt) {
+            for (int num : arrayBase) {
+                System.out.println(num);
+            }
+        }
+    }
+}
