847747e2b9da550616a773dda74c0425019acd50
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-06-17 09:31:19 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/JavaCore/Aintroducaoclassess/test/Carro_Objeto.java
+++ src/academy/devdojo/maratonajava/JavaCore/Aintroducaoclassess/test/Carro_Objeto.java
@@ -4,17 +4,18 @@ import academy.devdojo.maratonajava.JavaCore.Aintroducaoclassess.dominio.Carro;
 
 public class Carro_Objeto {
     public static void main(String[] args) {
-        Carro carro = new Carro();
-        carro.nome = "Volks";
-        carro.modelo = "Gol";
-        carro.ano = 2023;
-
+        Carro carro1 = new Carro();
         Carro carro2= new Carro();
+
+        carro1.nome = "Volks";
+        carro1.modelo = "Gol";
+        carro1.ano = 2023;
+
         carro2.nome = "Toyota";
         carro2.modelo = "Corolla";
         carro2.ano = 2023;
 
-        System.out.println("Nome " + carro.nome +" Modelo " + carro.modelo +" Ano "+carro.ano);
+        System.out.println("Nome " + carro1.nome +" Modelo " + carro1.modelo +" Ano "+carro1.ano);
         System.out.println("____________________________________________________");
         System.out.println("Nome " + carro2.nome +" Modelo " + carro2.modelo +" Ano "+carro2.ano);
     }
