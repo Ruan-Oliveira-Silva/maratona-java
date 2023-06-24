3c2d0252e90c5b9a1117205a6154bf871602b580
ruan <ruanolisilva25@gmail.com>
2023-06-24 09:55:05 -0300

    ... note

--- src/academy/devdojo/maratonajava/JavaCore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/JavaCore/Bintroducaometodos/dominio/Calculadora.java
@@ -12,4 +12,7 @@ public class Calculadora {
     public void multiplicaDoisNumeros(int num1, int num2){
         System.out.println(num1 * num2);
     }
+    public double divideDoisNumeros (int num1, double num2){
+        return(int) (num1/num2);
+    }
 }
---
+++ src/academy/devdojo/maratonajava/JavaCore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.JavaCore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.JavaCore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.divideDoisNumeros(20,2);
+        System.out.println(result);
+
+
+    }
+}
