acbc6e4fe0f79d1a5fd1cc5749ab1c0c7f013b2d
ruan <ruanolisilva25@gmail.com>
2023-06-23 21:21:20 -0300

    ... note

---
+++ src/academy/devdojo/maratonajava/JavaCore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.JavaCore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.JavaCore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10,20);
+    }
+}
