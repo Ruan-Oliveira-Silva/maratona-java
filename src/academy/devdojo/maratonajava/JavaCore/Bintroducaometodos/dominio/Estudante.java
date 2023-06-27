602b41d4c4f5d3b21cebf96eeb86d35de23527e1
ruan <ruanolisilva25@gmail.com>
2023-06-26 22:32:27 -0300

    ... note

---
+++ src/academy/devdojo/maratonajava/JavaCore/Bintroducaometodos/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.JavaCore.Bintroducaometodos.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ src/academy/devdojo/maratonajava/JavaCore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.JavaCore.Bintroducaometodos.dominio;
+
+public class ImpressoraEstudante {
+    public void imprime(Estudante estudante){
+        System.out.println("__________");
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+
+
+    }
+}
