1ee25cbfd6c2d876e1c930b94bb9cc1d1c6289ab
ruan <ruanolisilva25@gmail.com>
2023-07-03 19:36:04 -0300

    ... note

---
+++ src/academy/devdojo/maratonajava/JavaCore/Bintroducaometodos/test/PessoTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.JavaCore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.JavaCore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Ruan ");
+        pessoa.setIdade(-1);
+        pessoa.imprime();
+
+    }
+}
