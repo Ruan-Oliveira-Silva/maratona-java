6f10bb1ddb438d17f0dd36cd6b0ee3418b8325e9
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-07-05 20:12:52 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/JavaCore/Bintroducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/JavaCore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,26 +1,60 @@
 package academy.devdojo.maratonajava.JavaCore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media = 0;
 
-    public void imprime(){
+    public void imprime() {
         System.out.println(this.nome);
         System.out.println(this.idade);
-        if(salarios != null)
-       for(double salario: salarios){
-           System.out.print(salario + "  ");
-       }
-       imprimeMediaSalarial();
+        if (salarios != null)
+            for (double salario : salarios) {
+                System.out.print(salario + "  ");
+            }
+        imprimeMediaSalarial();
     }
-    public void imprimeMediaSalarial(){
-        double media = 0;
-        if(salarios != null )
-        for(double salario: salarios){
-            media += salario;
-        }
+
+    public void imprimeMediaSalarial() {
+
+        if (salarios != null)
+            for (double salario : salarios) {
+                media += salario;
+            }
         media /= salarios.length;
         System.out.println("\nMédia salarial " + media);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 }
